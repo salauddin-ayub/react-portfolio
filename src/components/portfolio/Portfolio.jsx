d7a0171f4120a4d50ed30447182f6413cf3a08cb
SalauddinAyub <ayub35-265@diu.edu.bd>
2022-03-05 13:23:29 +0600

    projece and testimonial section added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,67 @@
 import React from 'react';
 import './Portfolio.css';
+import dhopa from '../../assets/Smart_Dhopa.png';
+import ornament from '../../assets/ornament.PNG';
+import hospital from '../../assets/hospital.PNG';
+
+import port from '../../assets/port.PNG';
+
+
+const data = [
+    {
+        id: 1,
+        image: ornament,
+        title: 'Ornament Gallery',
+        github:'https://github.com',
+        demo:'http://ornamentgallery.herokuapp.com/'
+    },
+    {
+        id: 2,
+        image: hospital,
+        title: 'Hospital Apothecary',
+        github:'https://github.com',
+        demo:'http://hospital-apothecary.herokuapp.com/'
+    },
+    {
+        id: 3,
+        image: port,
+        title: 'Django Portfolio',
+        github:'https://github.com',
+        demo:'http://salauddinayub-portfolio.herokuapp.com/'
+    },
+    {
+        id: 4,
+        image: dhopa,
+        title: 'Smart Dhopa',
+        github:'https://github.com',
+        demo:'https://smart-dhopa-online-laundry-app.web.app/'
+    },
+]
 
 const Portfolio = () => {
     return (
         <section id="portfolio">
-            This is Portfolio
+            <h5>My Recent Work</h5>
+            <h2>Projects</h2>
+
+            <div className="container portfolio__container">
+                {
+                    data.map(({id, image, title, github, demo}) =>{
+                        return (
+                         <article key={id} className='portfolio__item'>
+                            <div className="portfolio__item-image">
+                               <img src={image} alt={title} />
+                            </div>
+                            <h3>{title}</h3>
+                            <div className="portfolio__item-cta">
+                            <a href={github} className='btn' target='_blank'>Github</a>
+                            <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                           </div>
+                         </article>
+                        )
+                    })
+                }
+            </div>
         </section>
     );
 };
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,11 +1,66 @@
 import React from 'react';
 import './Testimonials.css';
+import frnd1 from '../../assets/arzu.jpg';
+import frnd2 from '../../assets/joy.jpg';
+import frnd3 from '../../assets/siplo.jpg';
+
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const data =[
+    {
+        avatar:frnd1,
+        name: 'Anisur Rahman',
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aspernatur veritatis hic dolorem inventore sequi cumque dignissimos praesentium mollitia distinctio!'
+
+    },
+    {
+        avatar:frnd2,
+        name: 'Johirul Islam',
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aspernatur veritatis hic dolorem inventore sequi cumque dignissimos praesentium mollitia distinctio!'
+
+    },
+    {
+        avatar:frnd3,
+        name: 'Jahid Hasan',
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aspernatur veritatis hic dolorem inventore sequi cumque dignissimos praesentium mollitia distinctio!'
+
+    },
+]
 
 const Testimonials = () => {
     return (
         <section id="testimonials">
-        This is Testimonials
-    </section>
+        <h5>Review from others</h5>
+        <h2>testimonials</h2>
+        <Swiper className="container testimonials__container"
+        modules={[ Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}>
+            {
+                data.map(({avatar,name,review}, index) => {
+                    return (
+                   <SwiperSlide key={index} className="testimonial">
+                       <div className="client__avatar">
+                         <img src={avatar} alt="" />
+                       </div>
+                       <h5 classname="client__name">{name}</h5>
+                       <small className='client__review'>
+                       {review}
+                      </small>
+                   </SwiperSlide>
+                    )
+                })
+            }
+        </Swiper>
+        </section>
     );
 };
 
