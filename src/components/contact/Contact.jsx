fa64dad9baf02d6ca89f863181095bfab3e55f70
SalauddinAyub <ayub35-265@diu.edu.bd>
2022-03-06 00:31:38 +0600

    contact and footer section added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
 import './Contact.css';
+import {MdOutlineEmail} from 'react-icons/md';
+import {RiMessengerLine} from 'react-icons/ri';
+import {BsWhatsapp} from 'react-icons/bs'; 
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_cv4t6rv', 'service_cv4t6rv', form.current, 'drfGYQ6q6av6x-Jnl')
+
+        e.target.reset()
+      };
+
     return (
         <section id="contact">
-        This is Contact
+        <h5>Get in Touch</h5>
+        <h2>Contact me</h2>
+
+        <div className="container contact__container">
+            <div className="contact__options">
+                <article className="contact__option">
+                    <MdOutlineEmail className='contact__option-icon'/>
+                    <h4>Email</h4>
+                    <h5>salauddinnayub@gmail.com</h5>
+                    <a href="mailto:salauddinnayub@gmail.com" target="_blank">Send a message</a>
+                </article>
+                <article className="contact__option">
+                    <RiMessengerLine className='contact__option-icon'/>
+                    <h4>Messenger</h4>
+                    <h5>Salauddin Ayub</h5>
+                    <a href="https://m.me/leojrmax.max" target="_blank">Send a message</a>
+                </article>
+                <article className="contact__option">
+                    <BsWhatsapp className='contact__option-icon'/>
+                    <h4>Whatsapp</h4>
+                    <h5>+88 01910-546032</h5>
+                    <a href="https://api.whatsapp.com/send?phone+8801910546032" target="_blank">Send a message</a>
+                </article>
+            </div>
+            <form ref={form} onSubmit={sendEmail}>
+                <input type="text" name='name' placeholder='Your Full Name' required />
+                <input type="email" name='email' placeholder='Your Email' required />
+                <textarea name="message" rows="7" placeholder="Your Message" required ></textarea>
+                <button type='submit' className='btn btn-primary'>Send</button>
+            </form>
+        </div>
         </section>
     );
 };
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import './Footer.css';
+import {FaFacebook} from 'react-icons/fa';
+import {FaInstagram} from 'react-icons/fa';
+import {IoLogoTwitter} from 'react-icons/io'
 
 const Footer = () => {
     return (
-        <section id="about">
-        Footer
-        </section>
+       <footer>
+           <a href="#" className='footer__logo'>Ayub</a>
+
+           <ul className='permalinks'>
+               <li><a href="#">Home</a></li>
+               <li><a href="#about">About</a></li>
+               <li><a href="#experience">Experience</a></li>
+               <li><a href="#services">Service</a></li>
+               <li><a href="#portfolio">Portfolio</a></li>
+               <li><a href="#testimonials">Testimonials</a></li>
+               <li><a href="#contact">Contact</a></li>
+           </ul>
+           <div className="footer__socials">
+               <a href="https://facebook.com"><FaFacebook/></a>
+               <a href="https://instagram.com"><FaInstagram/></a>
+               <a href="https://twitter.com"><IoLogoTwitter/></a>
+
+           </div>
+           <div className="footer__copyright">
+               <small>&copy; Salauddin Ayub. All rights reserved.</small>
+           </div>
+       </footer>
     );
 };
 
